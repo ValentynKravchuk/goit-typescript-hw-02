37f883155995382738ad3773fb572e8df6cab469
ValentynKravchuk <167019009+ValentynKravchuk@users.noreply.github.com>
2024-12-26 19:15:50 +0100

    finish

--- src/components/Loader/Loader.tsx
+++ src/components/Loader/Loader.tsx
@@ -1,9 +1,9 @@
-import React from "react";
+import React, { FC } from "react";
 import { TailSpin } from "react-loader-spinner";
 import s from "./Loader.module.css";
 
-const Loader = () => (
-  <div className="Loader">
+const Loader: FC = () => (
+  <div className={s.loader}>
     <TailSpin color="#00BFFF" height={80} width={80} />
   </div>
 );
